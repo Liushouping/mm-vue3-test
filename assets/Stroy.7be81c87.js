6a34a65b73bc4d4222fc7cca24272d8b567c826d
liushouping <kose146352@gmail.com>
2022-09-23 00:28:45 +0800

    test

---
+++ assets/Stroy.7be81c87.js
@@ -0,0 +1,1 @@
+import{o,c as t,a as e,_ as r}from"./index.c2aa3f9a.js";import{F as a}from"./FooterTwo.8055b512.js";const s={class:"relative w-full h-screen bg-violet-500 overflow-hidden"},m={__name:"Stroy",setup(c){return(_,n)=>(o(),t("div",s,[e(r),e(a)]))}};export{m as default};
