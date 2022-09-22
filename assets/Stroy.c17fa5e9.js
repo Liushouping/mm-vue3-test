f795eb9805546206f56b94b180c97c56b444cd09
liushouping <kose146352@gmail.com>
2022-09-22 23:16:29 +0800

    Test

---
+++ assets/Stroy.c17fa5e9.js
@@ -0,0 +1,1 @@
+import{o,c as t,a as e,_ as r}from"./index.ec75968f.js";import{F as a}from"./FooterTwo.52deafa1.js";const s={class:"relative w-full h-screen bg-violet-500 overflow-hidden"},m={__name:"Stroy",setup(c){return(_,n)=>(o(),t("div",s,[e(r),e(a)]))}};export{m as default};
